e219f536ae0771df3b7d738c5da7a1ce73f3cda6
Björn <dexta.de@gmail.com>
2012-02-19 15:14:57 +0000

    "upgrade working blog"

--- README.js
+++ README.js
@@ -1,3 +1,5 @@
 var codeLog = 'Try some Workflow with home <-> github <-> c9.io';
 codeLog += 'there are some traps so dancing around ^^';
+codeLog += 'Delete some file on home and some different on c9.io';
+codeLog += 'looking good so on .... :)';
 
