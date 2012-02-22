2dbc19d373b11dcc577004cfb1e03816a184c8b4
Björn <dexta.de@gmail.com>
2012-02-22 22:25:21 +0100

    Loging the change

--- README.js
+++ README.js
@@ -3,3 +3,9 @@ codeLog += 'there are some traps so dancing around ^^';
 codeLog += 'Delete some file on home and some different on c9.io';
 codeLog += 'looking good so on .... :)';
 
+codeLog += '- 3days later:';
+codeLog += 'Add a wisper mode -> type /w username message';
+codeLog += 'Fix the Reload Problem -> app.js crash on F5';
+codeLog += 'Change some color, and make a darker theme ';
+codeLog += ' you are welcome to add some more theming ';
+
